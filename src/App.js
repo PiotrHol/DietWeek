9d9c7ec96dd58e911a8cc8be38bf1587cc80adbe
Piotr Hołub <piotrekholub@wp.pl>
2023-02-26 09:08:14 +0100

    Added routing to app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import "./scss/main.scss";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import { PrivateRoute } from "./PrivateRoute";
+import { Redirect } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>DietWeek app</p>
-      </header>
+    <div className="app">
+      <HashRouter basename="/">
+        <Switch>
+          <PrivateRoute exact path="/">
+            Home
+          </PrivateRoute>
+          <Route path="/login">
+            <div className="App">
+              <header className="App-header">
+                <p>DietWeek app</p>
+              </header>
+            </div>
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
+      </HashRouter>
     </div>
   );
 }
