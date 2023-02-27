24913e72eea4ec4fd21943527185a40517744926
Piotr Hołub <piotrekholub@wp.pl>
2023-02-27 19:41:30 +0100

    Added DesktopNotAvailable component to App component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,40 @@
+import React, { useEffect, useState } from "react";
 import "./scss/main.scss";
 import { HashRouter, Switch, Route } from "react-router-dom";
 import { PrivateRoute } from "./PrivateRoute";
 import { Redirect } from "react-router-dom";
+import { DesktopNotAvailable } from "./components/DesktopNotAvailable/DesktopNotAvailable";
 
 function App() {
+  const [isDesktopView, setIsDesktopView] = useState(
+    window.innerWidth > 768 ? true : false
+  );
+  useEffect(() => {
+    const handleResize = () =>
+      window.innerWidth > 768
+        ? setIsDesktopView(true)
+        : setIsDesktopView(false);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div className="app">
-      <HashRouter basename="/">
-        <Switch>
-          <PrivateRoute exact path="/">
-            Home
-          </PrivateRoute>
-          <Route path="/login">
-            <div className="App">
-              <header className="App-header">
-                <p>DietWeek app</p>
-              </header>
-            </div>
-          </Route>
-          <Route path="*">
-            <Redirect to="/" />
-          </Route>
-        </Switch>
-      </HashRouter>
+      {isDesktopView ? (
+        <DesktopNotAvailable />
+      ) : (
+        <HashRouter basename="/">
+          <Switch>
+            <PrivateRoute exact path="/">
+              Home
+            </PrivateRoute>
+            <Route path="/login"></Route>
+            <Route path="*">
+              <Redirect to="/" />
+            </Route>
+          </Switch>
+        </HashRouter>
+      )}
     </div>
   );
 }
