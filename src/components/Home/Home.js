05d6e3e4f86f1c0938ad3b83429449412d14fd8e
Piotr Hołub <piotrekholub@wp.pl>
2023-03-24 23:47:20 +0100

    Added routing in Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,32 @@
 import React from "react";
 import "./home.scss";
-import { Logo } from "../Logo/Logo";
 import { Menu } from "../Menu/Menu";
+import { HomeHeader } from "../HomeHeader/HomeHeader";
+import { Switch, useRouteMatch } from "react-router-dom";
+import { PrivateRoute } from "../../PrivateRoute";
 
 export const Home = () => {
+  let { path, url } = useRouteMatch();
   return (
     <div className="home">
-      <div className="home__header">
-        <h1 className="home__title">Strona główna</h1>
-        <Logo size={30} />
-      </div>
-      <Menu />
+      <Switch>
+        <PrivateRoute exact path={path}>
+          <HomeHeader title="Strona główna" />
+        </PrivateRoute>
+        <PrivateRoute path={`${path}/recipes`}>
+          <HomeHeader title="Przepisy" />
+        </PrivateRoute>
+        <PrivateRoute path={`${path}/weeks`}>
+          <HomeHeader title="Tygodnie" />
+        </PrivateRoute>
+        <PrivateRoute path={`${path}/shopping-list`}>
+          <HomeHeader title="Lista zakupów" />
+        </PrivateRoute>
+        <PrivateRoute path={`${path}/settings`}>
+          <HomeHeader title="Ustawienia" />
+        </PrivateRoute>
+      </Switch>
+      <Menu url={url} />
     </div>
   );
 };
