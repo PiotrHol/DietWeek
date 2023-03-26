5e374b53d9e3c3e3c584c051032e4ca57c1402bb
Piotr Hołub <piotrekholub@wp.pl>
2023-03-26 21:35:52 +0200

    Added fetching data to diet reducer

--- src/redux/actions/dietActions.js
+++ src/redux/actions/dietActions.js
@@ -1,3 +1,19 @@
-const typeName = {};
+const typeName = {
+  setUserData: "diet/setUserData",
+  clearUserData: "diet/clearUserData",
+};
 
-export { typeName };
+const setUserData = (payload) => {
+  return {
+    type: typeName.setUserData,
+    payload,
+  };
+};
+
+const clearUserData = () => {
+  return {
+    type: typeName.clearUserData,
+  };
+};
+
+export { typeName, setUserData, clearUserData };
--- src/redux/reducers/dietSlice.js
+++ src/redux/reducers/dietSlice.js
@@ -1,14 +1,42 @@
+import { typeName, setUserData } from "../actions/dietActions";
+import { getFirestore, getDocs, collection } from "firebase/firestore";
+import { app } from "../../firebase";
+
 const initialState = {
   recipes: [],
   weeks: [],
   activeWeek: null,
 };
 
+const fetchUserData = async (dispatch, getState) => {
+  const recipes = await getDocs(
+    collection(getFirestore(app), "users", getState().auth.userId, "recipes")
+  );
+  const recipesArray = [];
+  recipes.forEach((recipe) => {
+    recipesArray.push({
+      ...recipe.data(),
+      id: recipe.id,
+    });
+  });
+  dispatch(setUserData(recipesArray));
+};
+
 const dietReducer = (state = initialState, { type, payload }) => {
   switch (type) {
+    case typeName.setUserData:
+      return {
+        ...state,
+        recipes: payload.sort((a, b) => a.id - b.id),
+      };
+    case typeName.clearUserData:
+      return {
+        ...state,
+        recipes: [],
+      };
     default:
       return state;
   }
 };
 
-export { dietReducer };
+export { dietReducer, fetchUserData };
