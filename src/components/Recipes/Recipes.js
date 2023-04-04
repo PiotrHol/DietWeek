28ab72ee3b748472b5b55b6c649a4b59414fc282
Piotr Hołub <piotrekholub@wp.pl>
2023-04-04 21:56:24 +0200

    Added recipes list in Recipes component

--- src/components/Recipes/Recipes.js
+++ src/components/Recipes/Recipes.js
@@ -1,15 +1,21 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./recipes.scss";
 import { useSelector } from "react-redux";
 import { Button } from "../Button/Button";
 import { Popup } from "../Popup/Popup";
 import { EditRecipe } from "../EditRecipe/EditRecipe";
+import { FixedButton } from "../FixedButton/FixedButton";
 
 export const Recipes = () => {
-  const recipes = useSelector((state) => state.diet.recipes);
+  let recipes = useSelector((state) => state.diet.recipes);
+  const [recipesToShow, setRecipesToShow] = useState(recipes);
   const [showPopup, setShowPopup] = useState(false);
   const [popupTitle, setPopupTitle] = useState("");
   const [popupContent, setPopupContent] = useState(null);
+
+  useEffect(() => {
+    setRecipesToShow(recipes);
+  }, [recipes]);
 
   const handleAddRecipeBtn = () => {
     setPopupTitle("Dodaj przepis");
@@ -20,7 +26,39 @@ export const Recipes = () => {
   return (
     <div className="recipes">
       {recipes.length > 0 ? (
-        <div className="recipes__list"></div>
+        <>
+          <div className="recipes__list">
+            <div className="recipes__list-titles">
+              <div className="recipes__recipe-name">Nazwa</div>
+              <div className="recipes__recipe-category">Kategoria</div>
+              <div className="recipes__recipe-calories">Kalorie</div>
+              <div className="recipes__recipe-ingredients">Składniki</div>
+              <div className="recipes__recipe-description">Opis</div>
+            </div>
+            {recipesToShow.map((recipe) => (
+              <div key={recipe.id} className="recipes__recipe">
+                <div className="recipes__recipe-name">{recipe.name}</div>
+                <div className="recipes__recipe-category">
+                  {recipe.category}
+                </div>
+                <div className="recipes__recipe-calories">
+                  {recipe.calories} kcal
+                </div>
+                <div className="recipes__recipe-ingredients">
+                  {recipe.ingredients.length}
+                </div>
+                <div className="recipes__recipe-description">
+                  {recipe.description.slice(0, 100)}
+                </div>
+              </div>
+            ))}
+          </div>
+          <FixedButton
+            buttonText="Dodaj"
+            buttonTextSize={15}
+            buttonHandleClick={handleAddRecipeBtn}
+          />
+        </>
       ) : (
         <div className="recipes__empty">
           <div className="recipes__empty-text">Nie masz żadnych przepisów</div>
