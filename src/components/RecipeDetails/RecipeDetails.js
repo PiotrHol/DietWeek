ba91e589ea43d2fc3c116523f68b0e3851d612b6
Piotr Hołub <piotrekholub@wp.pl>
2023-04-06 20:15:37 +0200

    Created RecipeDetails component

---
+++ src/components/RecipeDetails/RecipeDetails.js
@@ -0,0 +1,102 @@
+import React from "react";
+import "./recipeDetails.scss";
+import { Button } from "../Button/Button";
+import classNames from "classnames";
+import { EditRecipe } from "../EditRecipe/EditRecipe";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+import { deleteRecipe } from "../../redux/actions/dietActions";
+import { getFirestore, doc, deleteDoc } from "firebase/firestore";
+import { app } from "../../firebase";
+
+export const RecipeDetails = ({
+  recipeId,
+  recipeName,
+  recipeCalories,
+  recipeCategory,
+  recipeIngredients,
+  recipeDescription,
+  changePopupTitle,
+  showEditRecipe,
+  closeHandler,
+}) => {
+  const userId = useSelector((state) => state.auth.userId);
+  const dispatch = useDispatch();
+  const handleEditBtn = () => {
+    changePopupTitle("Edytuj przepis");
+    showEditRecipe(
+      <EditRecipe
+        recipeId={recipeId}
+        recipeName={recipeName}
+        recipeCalories={recipeCalories}
+        recipeCategory={recipeCategory}
+        recipeIngredients={recipeIngredients}
+        recipeDescription={recipeDescription}
+        closeHandler={closeHandler}
+      />
+    );
+  };
+
+  const handleDeleteBtn = async () => {
+    try {
+      await deleteDoc(
+        doc(getFirestore(app), "users", userId, "recipes", recipeId)
+      );
+    } catch (error) {}
+    dispatch(deleteRecipe(recipeId));
+    closeHandler();
+  };
+
+  return (
+    <div className="recipe-details">
+      <div className="recipe-details__name-section">
+        <div className="recipe-details__title">
+          <div className="recipe-details__name">{recipeName}</div>
+          <div>{recipeCalories} kcal</div>
+        </div>
+        <div className="recipe-details__btn-section">
+          <Button
+            buttonStyle="secondary"
+            buttonText="Edytuj"
+            buttonTextSize={13}
+            buttonHandleClick={handleEditBtn}
+          />
+          <Button
+            buttonStyle="secondary"
+            buttonText="Usuń"
+            buttonTextSize={13}
+            buttonHandleClick={handleDeleteBtn}
+          />
+        </div>
+      </div>
+      <div className="recipe-details__section">
+        <div className="recipe-details__section-title">Składniki</div>
+        <div className="recipe-details__ingredients">
+          {recipeIngredients.length > 0 ? (
+            recipeIngredients.map((ingredient, index) => (
+              <div key={index} className="recipe-details__ingredient">
+                {ingredient.name} - {ingredient.quantity} {ingredient.unit}
+              </div>
+            ))
+          ) : (
+            <div className="recipe-details__section-text">Brak</div>
+          )}
+        </div>
+      </div>
+      <div
+        className={classNames(
+          "recipe-details__section recipe-details__description",
+          {
+            "recipe-details__description--ingredients":
+              recipeIngredients.length > 0,
+          }
+        )}
+      >
+        <div className="recipe-details__section-title">Przepis</div>
+        <div className="recipe-details__section-text">
+          {recipeDescription ? recipeDescription : "Brak"}
+        </div>
+      </div>
+    </div>
+  );
+};
