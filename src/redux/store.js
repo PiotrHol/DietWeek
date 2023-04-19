9fb6b6a152314eeb504c4dd97f196639b6b93a3c
Piotr Hołub <piotrekholub@wp.pl>
2023-04-19 18:35:57 +0200

    Added new reducer to redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import authReducer from "./reducers/authSlice";
 import { dietReducer } from "./reducers/dietSlice";
+import editedWeekReducer from "./reducers/editedWeekSlice";
 import { composeWithDevTools } from "redux-devtools-extension";
 import thunkMiddleware from "redux-thunk";
 
 const rootReducer = combineReducers({
   auth: authReducer,
   diet: dietReducer,
+  editedWeek: editedWeekReducer,
 });
 
 const composedEnhancer = composeWithDevTools(applyMiddleware(thunkMiddleware));
