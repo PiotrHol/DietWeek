42043b029947ed48c52f157f485c9763a0a4ae8d
Piotr Hołub <piotrekholub@wp.pl>
2023-05-07 18:14:18 +0200

    Created ActiveWeek component

---
+++ src/components/ActiveWeek/ActiveWeek.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import "./activeWeek.scss";
+import { useSelector } from "react-redux";
+import { Week } from "../Week/Week";
+import { Popup } from "../Popup/Popup";
+import { RecipeDetails } from "../RecipeDetails/RecipeDetails";
+
+export const ActiveWeek = () => {
+  const activeWeekData = useSelector((state) => state.diet.activeWeek);
+  const activeWeek = useSelector((state) =>
+    state.diet.weeks.filter(
+      (week) => activeWeekData && week.id === activeWeekData.id
+    )
+  )[0];
+  const recipes = useSelector((state) => state.diet.recipes);
+  const [showPopup, setShowPopup] = useState(false);
+  const [popupTitle, setPopupTitle] = useState("");
+  const [popupContent, setPopupContent] = useState(null);
+
+  const showRecipe = (recipeId) => {
+    const recipeToShow = recipes.filter((recipe) => recipe.id === recipeId)[0];
+    setPopupTitle(recipeToShow.category);
+    setPopupContent(
+      <RecipeDetails
+        recipeId={recipeToShow.id}
+        recipeName={recipeToShow.name}
+        recipeCalories={recipeToShow.calories}
+        recipeCategory={recipeToShow.category}
+        recipeIngredients={recipeToShow.ingredients}
+        recipeDescription={recipeToShow.description}
+        withOptions={false}
+      />
+    );
+    setShowPopup(true);
+  };
+
+  return (
+    <div className="active-week">
+      {activeWeek ? (
+        <div className="active-week__week">
+          <Week
+            isEdit={false}
+            isPopup={false}
+            weekName={activeWeek.name}
+            weekDays={activeWeek}
+            handleShowRecipe={showRecipe}
+          />
+        </div>
+      ) : (
+        <div className="active-week__empty">
+          <div className="active-week__empty-text">
+            Nie masz wybranego tygodnia diety
+          </div>
+        </div>
+      )}
+      {showPopup && (
+        <Popup
+          popupTitle={popupTitle}
+          popupCloseHandler={() => setShowPopup(false)}
+        >
+          {popupContent}
+        </Popup>
+      )}
+    </div>
+  );
+};
