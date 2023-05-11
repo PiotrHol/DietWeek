a2efbd35e9d49e82c9f3a2e8cb30247d39f02d9d
Piotr Hołub <piotrekholub@wp.pl>
2023-05-11 22:02:26 +0200

    Updated ShoppingList component

--- src/components/ShoppingList/ShoppingList.js
+++ src/components/ShoppingList/ShoppingList.js
@@ -1,26 +1,74 @@
 import React from "react";
 import "./shoppingList.scss";
 import { useSelector } from "react-redux";
+import classNames from "classnames";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { useDispatch } from "react-redux";
+import { checkIngredient } from "../../redux/actions/dietActions";
+import { getFirestore, doc, updateDoc } from "firebase/firestore";
+import { app } from "../../firebase";
 
 export const ShoppingList = () => {
   const activeWeek = useSelector((state) => state.diet.activeWeek);
+  const ingredientsList = activeWeek
+    ? Object.keys(activeWeek.ingredients).map((ingredientKey) => {
+        return {
+          ...activeWeek.ingredients[ingredientKey],
+          id: ingredientKey,
+        };
+      })
+    : null;
+  const sortIngredientsList = ingredientsList
+    ? ingredientsList.sort((a, b) => a.check - b.check)
+    : [];
+  const dispatch = useDispatch();
+  const userId = useSelector((state) => state.auth.userId);
+
+  const ingredientClickHandler = async (ingredientKey, isCheck) => {
+    const valueToSet = !isCheck;
+    try {
+      await updateDoc(doc(getFirestore(app), "users", userId), {
+        [`activeWeekIngredients.${ingredientKey}.check`]: valueToSet,
+      });
+    } catch (error) {}
+    dispatch(checkIngredient(ingredientKey, valueToSet));
+  };
 
   return (
     <div className="shopping-list">
-      {activeWeek && activeWeek.ingredients ? (
+      {sortIngredientsList.length > 0 ? (
         <div className="shopping-list__list">
-          {Object.keys(activeWeek.ingredients).map((ingredientKey) => (
-            <div key={ingredientKey} className="shopping-list__ingredient">
+          {sortIngredientsList.map((ingredient) => (
+            <div
+              key={ingredient.id}
+              className={classNames("shopping-list__ingredient", {
+                "shopping-list__ingredient--active": ingredient.check,
+              })}
+              onClick={() =>
+                ingredientClickHandler(ingredient.id, ingredient.check)
+              }
+            >
               <div className="shopping-list__ingredient-content">
                 <div className="shopping-list__ingredient-value">
-                  {activeWeek.ingredients[ingredientKey].name}
+                  {ingredient.name}
                 </div>
                 <div className="shopping-list__ingredient-value">
-                  - {activeWeek.ingredients[ingredientKey].quantity}
+                  - {ingredient.quantity}
                 </div>
                 <div className="shopping-list__ingredient-value">
-                  {activeWeek.ingredients[ingredientKey].unit}
+                  {ingredient.unit}
                 </div>
+              </div>
+              <div
+                className={classNames("shopping-list__ingredient-check", {
+                  "shopping-list__ingredient-check--active": ingredient.check,
+                })}
+              >
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  className="shopping-list__ingredient-check-icon"
+                />
               </div>
             </div>
           ))}
