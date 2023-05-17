887c22c21a52d12b1ad5719871991318e58d2117
Piotr Hołub <piotrekholub@wp.pl>
2023-05-17 21:34:21 +0200

    Created Settings component

---
+++ src/components/Settings/Settings.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./settings.scss";
+import { ChangePassword } from "../ChangePassword/ChangePassword";
+
+export const Settings = () => {
+  return (
+    <div className="settings">
+      <ChangePassword />
+    </div>
+  );
+};
