2666e4d1c92d0567190900b732617c376ecbb9e5
Piotr Hołub <piotrekholub@wp.pl>
2023-05-28 23:07:35 +0200

    Updated index file

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 // eslint-disable-next-line
 import { app } from "./firebase";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -14,3 +15,5 @@ root.render(
     </Provider>
   </React.StrictMode>
 );
+
+serviceWorkerRegistration.register();
