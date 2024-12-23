e1c049b07f0b7420d729342bd66e771a12782f0e
Piotr Hołub <piotrekholub@wp.pl>
2024-12-23 20:30:27 +0100

    Added Loader component using

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,13 +1,17 @@
 import { Route, Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { Loader } from "./components/Loader/Loader";
 
 export const PrivateRoute = ({ children, ...rest }) => {
   const isAuth = useSelector((state) => state.auth.userId);
+  const isLoading = useSelector((state) => state.auth.checkingUser);
   return (
     <Route
       {...rest}
       render={({ location }) =>
-        isAuth ? (
+        isLoading ? (
+          <Loader />
+        ) : isAuth ? (
           children
         ) : (
           <Redirect
